685b5e9bbc6eae472ecfe34828a377a0720217a7
yanggang <yanggang@houduniot.com>
2019-04-23 17:35:56 +0800

    2019年4月23日17:35:45  代码优化

--- app/src/main/java/ygang/com/widgetlinechart/LineChart.java
+++ app/src/main/java/ygang/com/widgetlinechart/LineChart.java
@@ -1,7 +1,6 @@
 package ygang.com.widgetlinechart;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -40,23 +39,20 @@ public class LineChart extends View {
     //背景颜色
     private int bgColor = Color.parseColor("#ffffff");
 
-    //原点坐标最大x
-    private float ori_x;
-
-    //第一个点的坐标
-    private float first_x = 50;
-
-    //第一个点的坐标最小x，和最大x坐标
-    private float ori_min_x, ori_max_x;
+    //原点坐标x
+    private float ori_x = 80;
 
     //原点坐标y
     private float ori_y;
 
+    //y轴距离底部空隙间距
+    private float bottom_y = 80;
+
     //x的刻度值长度  默认 40
-    private int xScale = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 80, getResources().getDisplayMetrics());
+    private int xScale = 80;
 
     //y的刻度值
-    private int yScale = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 55, getResources().getDisplayMetrics());
+    private int yScale = 80;
 
     //x刻度
     private String[] xLables = {"1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月", "时间",};
@@ -72,14 +68,6 @@ public class LineChart extends View {
 
     //y坐标的最远坐标
     private int minY_X, minY_Y;
-
-    //x轴最远的坐标值
-    private float x_last_x, x_last_y;
-
-    //y轴最远的坐标值
-    private float y_last_x, y_last_y;
-
-    private double[] dataValues = {40, 80, 120, 160, 200, 140};
 
     //滑动时候，上次手指的x坐标
     private float starX;
@@ -135,29 +123,23 @@ public class LineChart extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        Log.i("ygang", "lineview===onMeasure widthMeasureSpec = " + widthMeasureSpec + " heightMeasureSpec = " + heightMeasureSpec);
+//        Log.i("ygang", "lineview===onMeasure widthMeasureSpec = " + widthMeasureSpec + " heightMeasureSpec = " + heightMeasureSpec);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+//        Log.v("lineview===onMeasure", "ondraw");
         width = canvas.getWidth();
         height = canvas.getHeight();
 
-        //原点坐标最大x
-        ori_x = 80;
         //原点坐标y
         ori_y = height - 80;
 
         //x坐标轴中最远的坐标值
-        maxX_X = width - 50;
-        minY_Y = 50;
+        maxX_X = width;
+        minY_Y = 80;
 
-        //第一个点的坐标最小x，和最大x坐标
-        ori_min_x = width - 50 - 40 - dataValues.length * xScale;
-        first_x = ori_x;
-        ori_max_x = first_x;
         Log.i("ygang", "lineview===onMeasure width = " + width + " height = " + height);
 
         drawXYLine(canvas);
@@ -172,22 +154,18 @@ public class LineChart extends View {
         paint.setStrokeWidth(xyWidth);
         paint.setTextSize(xyTextSize);
         //绘制x轴
-        float max = first_x + (xLables.length - 1) * xScale + 50;
+        float max = ori_x + (xLables.length - 1) * xScale + 80;//80是 多出来的距离
         if (max > maxX_X) {
-            max = getMeasuredWidth();
+            max = width;
         }
-        x_last_x = max;
-        x_last_y = ori_y;
-        canvas.drawLine(first_x, ori_y, max, ori_y, paint);
+        canvas.drawLine(ori_x, ori_y, max, ori_y, paint);
 
         //绘制y轴
-        float min = ori_y - (yLables.length - 1) * yScale - 50;
+        float min = ori_y - (yLables.length - 1) * yScale - 80;//80是 多出来的距离
         if (min < minY_Y) {
             min = minY_Y;
         }
-        y_last_x = first_x;
-        y_last_y = min;
-        canvas.drawLine(first_x, ori_y, first_x, min, paint);
+        canvas.drawLine(ori_x, ori_y, ori_x, min, paint);
         //绘画x轴的刻度
         drawXLablePoints(canvas, paint);
         //绘画y轴的刻度
@@ -197,17 +175,22 @@ public class LineChart extends View {
     //绘画x轴的刻度
     private void drawXLablePoints(Canvas canvas, Paint paint) {
         for (int i = 0; i < xLables.length; i++) {
-            canvas.drawText(xLables[i], first_x + i * 75, ori_y + 30, paint);
+            canvas.drawText(xLables[i], ori_x + i * xScale, ori_y + 30, paint);
         }
     }
 
 
-    //绘画y轴的刻度s
+    //绘画y轴的刻度
     private void drawYLablePoints(Canvas canvas, Paint paint) {
         for (int i = 0; i < yLables.length; i++) {
             Rect rect = new Rect();
             paint.getTextBounds(yLables[i], 0, yLables[i].length(), rect);
-            canvas.drawText(yLables[i], first_x - 7 - rect.width(), height - (first_x + i * 75), paint);
+            if (i < 3 || (i > 4 && i < 7)) {
+                paint.setColor(Color.parseColor("#D81B60"));
+            } else {
+                paint.setColor(Color.parseColor("#BCBCBC"));
+            }
+            canvas.drawText(yLables[i], ori_x - 7 - rect.width(), height - (bottom_y + i * yScale), paint);
         }
     }
 
@@ -218,11 +201,12 @@ public class LineChart extends View {
             paint.setColor(lineColor);
             paint.setAntiAlias(true);
             paint.setStrokeWidth(xyWidth);
-            //75/20=3.75(y轴)
+            paint.setTextSize(xyTextSize);
+            //80/20=4(y轴)
             for (int i = 0; i < yValues.length; i++) {
                 if (i == 0) {
                     float xI = Float.parseFloat(yValues[i]);
-                    canvas.drawLine(first_x, (float) (height - (first_x + xI * 3.75)), first_x, (float) (height - (first_x + xI * 3.75)), paint);
+                    canvas.drawLine(ori_x, (float) (height - (bottom_y + xI * 4)), ori_x, (float) (height - (bottom_y + xI * 4)), paint);
                 } else {
                     float x1 = Float.parseFloat(yValues[i - 1]);
 
@@ -233,8 +217,8 @@ public class LineChart extends View {
 
                     Rect rect1 = new Rect();
                     paint.getTextBounds(xLables[i - 1], 0, xLables[i - 1].length(), rect1);
-                    float y1 = (float) (height - (first_x + x1 * 3.75));
-                    float y2 = (float) (height - (first_x + x2 * 3.75));
+                    float y1 = (float) (height - (bottom_y + x1 * 4));
+                    float y2 = (float) (height - (bottom_y + x2 * 4));
                     if (y1 != 0) {
                         // -8为了y坐标的点在文字中间
                         y1 = y1 - 8;
@@ -244,11 +228,9 @@ public class LineChart extends View {
                         y2 = y2 - 8;
                     }
                     if (i == 1) {
-                        canvas.drawLine(first_x + (i - 1) * 75, y1, first_x + i * 75 + rect.width(), y2, paint);
-                        Log.i("ygang", "lineview===onMeasure 坐标 = " + first_x + (i - 1) * 75 + " ， " + first_x + i * 75 + rect.width());
+                        canvas.drawLine(ori_x + (i - 1) * xScale, y1, ori_x + i * xScale + rect.width() / 2, y2, paint);
                     } else {
-                        canvas.drawLine(first_x + (i - 1) * 75 + rect1.width(), y1, first_x + i * 75 + rect.width(), y2, paint);
-                        Log.i("ygang", "lineview===onMeasure 坐标 = " + first_x + (i - 1) * 75 + rect1.width() + " ， " + first_x + i * 75 + rect.width());
+                        canvas.drawLine(ori_x + (i - 1) * xScale + rect1.width() / 2, y1, ori_x + i * xScale + rect.width() / 2, y2, paint);
                     }
 
                 }
@@ -259,8 +241,33 @@ public class LineChart extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+//        if (((yValues.length - 1) * xScale) < (maxX_X - ori_x)) {
+//            return false;
+//        }
         //事件的拖动
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                starX = event.getX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float distance = event.getX() - starX;
+                starX = event.getX();
+//                if (ori_x + distance > width) {
+//                    Log.v("tagtag", "111");
+//                    ori_x = bottom_y;
+//                } else if (ori_x + distance < bottom_y) {
+//                    Log.v("tagtag", "222");
+//                    ori_x = bottom_y;
+//                } else {
+                    Log.i("lineview===onMeasure", "333");
+                    ori_x = (int) (ori_x + distance);
+//                }
+                invalidate();
+                break;
+
+        }
+        return true;
+
     }
 
     public int getXyColor() {
@@ -325,30 +332,6 @@ public class LineChart extends View {
 
     public void setOri_x(int ori_x) {
         this.ori_x = ori_x;
-    }
-
-    public float getFirst_x() {
-        return first_x;
-    }
-
-    public void setFirst_x(int first_x) {
-        this.first_x = first_x;
-    }
-
-    public float getOri_min_x() {
-        return ori_min_x;
-    }
-
-    public void setOri_min_x(int ori_min_x) {
-        this.ori_min_x = ori_min_x;
-    }
-
-    public float getOri_max_x() {
-        return ori_max_x;
-    }
-
-    public void setOri_max_x(int ori_max_x) {
-        this.ori_max_x = ori_max_x;
     }
 
     public float getOri_y() {
@@ -423,46 +406,6 @@ public class LineChart extends View {
         this.minY_Y = minY_Y;
     }
 
-    public float getX_last_x() {
-        return x_last_x;
-    }
-
-    public void setX_last_x(int x_last_x) {
-        this.x_last_x = x_last_x;
-    }
-
-    public float getX_last_y() {
-        return x_last_y;
-    }
-
-    public void setX_last_y(int x_last_y) {
-        this.x_last_y = x_last_y;
-    }
-
-    public float getY_last_x() {
-        return y_last_x;
-    }
-
-    public void setY_last_x(int y_last_x) {
-        this.y_last_x = y_last_x;
-    }
-
-    public float getY_last_y() {
-        return y_last_y;
-    }
-
-    public void setY_last_y(int y_last_y) {
-        this.y_last_y = y_last_y;
-    }
-
-    public double[] getDataValues() {
-        return dataValues;
-    }
-
-    public void setDataValues(double[] dataValues) {
-        this.dataValues = dataValues;
-    }
-
     public float getStarX() {
         return starX;
     }
@@ -493,5 +436,13 @@ public class LineChart extends View {
 
     public void setyValues(String[] yValues) {
         this.yValues = yValues;
+    }
+
+    public float getBottom_y() {
+        return bottom_y;
+    }
+
+    public void setBottom_y(float bottom_y) {
+        this.bottom_y = bottom_y;
     }
 }
